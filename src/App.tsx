a77894f809af06400d2e426f62ec953875fae644
Luiz Fernando Vital <luizfvital@gmail.com>
2020-09-11 16:33:40 -0300

    Added AuthContext and Axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
 import GlobalStyles from './styles/global'
-// import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+// import SignUp from './pages/SignUp'
+
+import { AuthProvider } from './context/AuthContext'
 
 const App: React.FC = () => {
   return (
     <>
-      <SignUp />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
+
       <GlobalStyles />
     </>
   )
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,24 +1,60 @@
-import React from 'react'
+import React, { useRef, useCallback, useContext } from 'react'
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 import { Form } from '@unform/web'
+import { FormHandles } from '@unform/core'
+import * as Yup from 'yup'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
 import { Container, Content, Background } from './styles'
 
+import getValidaitonErrors from '../../utils/getValidationErrors'
+
+import { AuthContext } from '../../context/AuthContext'
+
 import logoImg from '../../assets/logo.svg'
 
+interface SignInFormData {
+  email: string
+  password: string
+}
+
 const SignIn: React.FC = () => {
-  const handleSubmit = (data: object) => {
-    console.log(data)
-  }
+  const formRef = useRef<FormHandles>(null)
+  const { signIn } = useContext(AuthContext)
+
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({})
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('Email obrigatório')
+            .email('Digite um email válido'),
+          password: Yup.string().min(6, 'Senha obrigatória'),
+        })
+
+        await schema.validate(data, {
+          abortEarly: false,
+        })
+
+        signIn({ email: data.email, password: data.password })
+      } catch (error) {
+        const errors = getValidaitonErrors(error)
+        formRef.current?.setErrors(errors)
+      }
+    },
+    [signIn],
+  )
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
 
           <Input name="email" icon={FiMail} placeholder="E-mail" />
